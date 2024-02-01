fa8dddd5d07ae74d15eaabd6e7e7f5173d76f5ac
Chris Sutton <chrissutton1789@gmail.com>
2024-02-01 14:25:16 +0000

    Error when call migrate for exerpt field. Deleted migration files, reset data base and applied again

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,7 @@ class Post(models.Model):
     content = models.TextField(default="")
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
     
   
     
