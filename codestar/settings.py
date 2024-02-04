c7f413bccd8bfd198a958783dbd6b78b8535b876
Chris Sutton <chrissutton1789@gmail.com>
2024-02-04 20:40:47 +0000

    Building the homepage with Django generic views

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-chrissuttondev-ittbwt-zj6nz8q5pcy.ws-eu107.gitpod.io',
+ALLOWED_HOSTS = ['8000-chrissuttondev-ittbwt-wqiwodemkl3.ws-eu108.gitpod.io',
 '.herokuapp.com']
 
 
