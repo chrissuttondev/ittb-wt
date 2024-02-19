ccd89135b019390a2ff86154edf5b919eec7a4ed
Chris Sutton <chrissutton1789@gmail.com>
2024-02-19 22:44:14 +0000

    Add comment CRUD

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True    
+DEBUG = False    
 
 ALLOWED_HOSTS = ['8000-chrissuttondev-ittbwt-tg3fw5f3m0k.ws-eu108.gitpod.io',
 '.herokuapp.com']
--- static/js/script.js
+++
@@ -1,1 +0,0 @@
-console.log("Ey Buddy!");
